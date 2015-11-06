bd1981653891d313c81efd5e79474f2f7b8e35d8
george.costa <geosohh@gmail.com>
2015-11-06 10:12:27 -0200

    update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
-rmdir "C:\Program Files\apache-tomcat-8.0.28\webapps\ROOT\kvmclient\lib" /s /q
-del /q /f "C:\Program Files\apache-tomcat-8.0.28\webapps\ROOT\kvmclient\*"
-copy "%JENKINS_HOME%\jobs\%JOB_NAME%\builds\%BUILD_NUMBER%\com.kvm$kvmclient\archive\com.kvm\kvmclient\0.0.1-SNAPSHOT\kvmclient-0.0.1-SNAPSHOT.jar" "C:\Program Files\apache-tomcat-8.0.28\webapps\ROOT\kvmclient\kvmclient.jar"
-(robocopy %WORKSPACE%\target\lib "C:\Program Files\apache-tomcat-8.0.28\webapps\ROOT\kvmclient\lib" /E) ^& IF %ERRORLEVEL% LEQ 1 exit 0
+rmdir "%TOMCAT%\webapps\ROOT\kvmclient\lib" /s /q
+del /q /f "%TOMCAT%\webapps\ROOT\kvmclient\*"
+copy "%JENKINS_HOME%\jobs\%JOB_NAME%\builds\%BUILD_NUMBER%\com.kvm$kvmclient\archive\com.kvm\kvmclient\0.0.1-SNAPSHOT\kvmclient-0.0.1-SNAPSHOT.jar" "%TOMCAT%\webapps\ROOT\kvmclient\kvmclient.jar"
+(robocopy %WORKSPACE%\target\lib "%TOMCAT%\webapps\ROOT\kvmclient\lib" /E) ^& IF %ERRORLEVEL% LEQ 1 exit 0
